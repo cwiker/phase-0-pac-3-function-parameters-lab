694e419bc105560932cfb6ddb81c98b56829ff84
Chris Wiker <cwiker7@gmail.com>
2021-11-10 23:40:22 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(firstName = "Josh") {
+    return `Hi, my name is ${firstName}.`;
+}
+
+ function introductionWithLanguage(name = "Josh", language = "Javascript") {
+     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+ }   
+
+ function introductionWithLanguageOptional(name = "Gracie", language = "Javascript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+} 
